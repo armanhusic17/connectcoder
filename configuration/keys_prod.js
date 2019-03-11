9eed2899381b56350eb5718535778a60e932a95a
armanhusic17 <ahusic@mail.sfsu.edu>
2019-03-11 12:23:22 -0700

    Full Webapp is finished, deploying to heroku and firebase has failed. Redeploying to heroku and working on fixing issue.

--- configuration/keys_prod.js
+++ configuration/keys_prod.js
@@ -2,3 +2,8 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY
 };
+
+var config = {
+  apiKey: "AIzaSyB0IoSKKB1G5rJP0d2sky6-tKHSXWf1z_I",
+  authDomain: "connectcoder-48c1c.firebaseapp.com"
+};
