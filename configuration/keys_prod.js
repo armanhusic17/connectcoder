5039a659487b395d689e3cbfc19ba023d6f647ed
armanhusic17 <ahusic@mail.sfsu.edu>
2019-03-17 09:47:20 -0700

    Cleaning up, double checking code.

--- configuration/keys_prod.js
+++ configuration/keys_prod.js
@@ -3,7 +3,7 @@ module.exports = {
   secretOrKey: process.env.SECRET_OR_KEY
 };
 
-var config = {
-  apiKey: "AIzaSyB0IoSKKB1G5rJP0d2sky6-tKHSXWf1z_I",
-  authDomain: "connectcoder-48c1c.firebaseapp.com"
-};
+// var config = {
+//   apiKey: "AIzaSyB0IoSKKB1G5rJP0d2sky6-tKHSXWf1z_I",
+//   authDomain: "connectcoder-48c1c.firebaseapp.com"
+// };
