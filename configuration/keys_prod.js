7c937b23bd300f1aff4c16576b39769ab556ab74
armanhusic17 <ahusic@mail.sfsu.edu>
2019-03-19 19:20:52 -0700

    Cleaning up codebase.

--- configuration/keys_prod.js
+++ configuration/keys_prod.js
@@ -2,8 +2,3 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   SECRET_OR_KEY: process.env.SECRET_OR_KEY
 };
-
-// var config = {
-//   apiKey: "AIzaSyB0IoSKKB1G5rJP0d2sky6-tKHSXWf1z_I",
-//   authDomain: "connectcoder-48c1c.firebaseapp.com"
-// };
